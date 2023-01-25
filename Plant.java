1578a2e422dc28a96f94621662f76b6166244279
sean-morris <sean.smorris@gmail.com>
2023-01-25 00:08:35 +0000

    hw

---
+++ Plant.java
@@ -0,0 +1,23 @@
+public class Plant{
+    private int age;
+    private String name;
+    private double height;
+
+    public Plant (String name){
+          age = 0;
+          this.name = name;
+          height = 0;
+    }
+
+    public String type(){
+         return "I am a plant and my name is: " + this.name;
+    }
+
+    public void increaseAge(int years){
+         age +=years;
+    }
+
+    public int age(){
+        return age;
+    }
+}
